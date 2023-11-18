a6a241f24b2005f4a2d014ca80da1468b783e159
DaniiB26 <danyspace12@gmail.com>
2023-11-18 17:24:54 +0200

    Modified Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,53 @@
-import {
-  IonHeader,
-  IonToolbar,
-  IonText,
-  IonButton,
-  IonIcon, IonMenuButton, IonMenuToggle,
-} from "@ionic/react";
-import classes from "./Header.module.css";
-import { personCircleOutline, searchCircleOutline } from "ionicons/icons";
-import { useContext } from "react";
-import { AuthContext } from "../context/AuthProvider";
-import {Link} from "react-router-dom";
+// Header.tsx
+import React, { useContext, useEffect, useState } from 'react';
+import { IonHeader, IonToolbar, IonIcon } from '@ionic/react';
+import { personCircleOutline, searchCircleOutline } from 'ionicons/icons';
+import { useHistory } from 'react-router-dom';
+import { getUserByUsername} from "../requests/userService";
+import { AuthContext } from '../context/AuthProvider';
+import classes from './Header.module.css';
 
-function Header() {
-  const {username} = useContext(AuthContext);
+const Header: React.FC = () => {
+  const { username } = useContext(AuthContext);
+  const history = useHistory();
+  const [userId, setUserId] = useState<number | null>(null);
+
+  useEffect(() => {
+    const fetchUserId = async () => {
+      try {
+        const response = await getUserByUsername(username);
+        setUserId(response.data.id);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    fetchUserId();
+  }, [username]);
+
+  const redirectToUserProfile = () => {
+    if (userId) {
+      history.push(`/user/${userId}`);
+    } else {
+      console.error('User ID not available.');
+    }
+  };
 
   return (
-    <IonHeader class={classes.header}>
-      <IonToolbar>
-        <div className={classes.headerContent}>
-          <IonMenuButton></IonMenuButton>
+      <IonHeader>
+        <IonToolbar>
           <div>
-            <Link to={`/user/${username}`} className={classes.button}>
-              <IonIcon icon={personCircleOutline} />
-            </Link>
+            <IonIcon
+                icon={personCircleOutline}
+                className={classes.button}
+                onClick={redirectToUserProfile}
+            />
             <IonIcon icon={searchCircleOutline} className={classes.button} />
           </div>
-        </div>
-      </IonToolbar>
-    </IonHeader>
+        </IonToolbar>
+      </IonHeader>
   );
-}
+};
 
 export default Header;
+
