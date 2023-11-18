c20a14e55153655422ac84bbfa41c5a674054748
Andrei POPOVICIU <apopoviciu@bitdefender.biz>
2023-11-18 16:55:10 +0200

    refactoring in order to use the footer component

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -1,34 +1,30 @@
-import { IonContent, IonPage, IonFooter, IonIcon } from "@ionic/react";
+import { IonContent, IonPage } from "@ionic/react";
 import RecipeCard from "../components/RecipeCard";
 import Header from "../components/Header";
-import { addCircleOutline } from "ionicons/icons";
 import classes from "./Feed.module.css";
 
 import Drawer from "../components/Drawer";
 import useRecipes from "../hooks/useRecipes";
+import Footer from "../components/Footer";
 
 const RecipeFeed: React.FC = () => {
   const { recipes, loading, error } = useRecipes();
 
   return (
     <>
-      <Drawer contentId="main-content"/>
+      <Drawer contentId="main-content" />
       <IonPage id="main-content">
-        <Header/>
+        <Header />
         <IonContent fullscreen className={classes.content}>
           <div className={classes.recipes}>
             {recipes.map((recipe) => (
-              <RecipeCard recipe={recipe}/>
+              <RecipeCard recipe={recipe} />
             ))}
           </div>
         </IonContent>
-        <IonFooter className={classes.footer} translucent={true}>
-          <IonIcon icon={addCircleOutline} className={classes.addButton}/>
-        </IonFooter>
+        <Footer />
       </IonPage>
-
     </>
-
   );
 };
 
